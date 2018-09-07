f94bc31badb2139649a32693d9dd64cc359ca63c
BennoGAP <mail@groebl.org>
2018-09-07 20:53:54 +0200

    Implement LettersToDigits

--- presentation/src/main/java/org/groebl/sms/feature/bluetooth/common/BluetoothNotificationFilter.java
+++ presentation/src/main/java/org/groebl/sms/feature/bluetooth/common/BluetoothNotificationFilter.java
@@ -10,10 +10,11 @@ import android.preference.PreferenceManager;
 import android.service.notification.StatusBarNotification;
 import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
+import android.util.SparseIntArray;
+
+import com.vdurmont.emoji.EmojiParser;
 
 import org.groebl.sms.BuildConfig;
-import org.groebl.sms.feature.bluetooth.common.BluetoothHelper;
-import com.vdurmont.emoji.EmojiParser;
 
 public class BluetoothNotificationFilter {
 
@@ -27,6 +28,55 @@ public class BluetoothNotificationFilter {
     private static String removeDirectionChars(String text) {
         return text.replaceAll("[\u202A|\u202B|\u202C|\u200B]", "");
     }
+
+    private static String convertKeypadLettersToDigits(String input) {
+        if (input == null) {
+            return input;
+        }
+        int len = input.length();
+        if (len == 0) {
+            return input;
+        }
+
+        char[] out = input.toCharArray();
+
+        for (int i = 0; i < len; i++) {
+            char c = out[i];
+            // If this char isn't in KEYPAD_MAP at all, just leave it alone.
+            out[i] = (char) KEYPAD_MAP.get(c, c);
+        }
+
+        return new String(out);
+    }
+
+
+    private static final SparseIntArray KEYPAD_MAP = new SparseIntArray();
+    static {
+        KEYPAD_MAP.put('a', '2'); KEYPAD_MAP.put('b', '2'); KEYPAD_MAP.put('c', '2');
+        KEYPAD_MAP.put('A', '2'); KEYPAD_MAP.put('B', '2'); KEYPAD_MAP.put('C', '2');
+
+        KEYPAD_MAP.put('d', '3'); KEYPAD_MAP.put('e', '3'); KEYPAD_MAP.put('f', '3');
+        KEYPAD_MAP.put('D', '3'); KEYPAD_MAP.put('E', '3'); KEYPAD_MAP.put('F', '3');
+
+        KEYPAD_MAP.put('g', '4'); KEYPAD_MAP.put('h', '4'); KEYPAD_MAP.put('i', '4');
+        KEYPAD_MAP.put('G', '4'); KEYPAD_MAP.put('H', '4'); KEYPAD_MAP.put('I', '4');
+
+        KEYPAD_MAP.put('j', '5'); KEYPAD_MAP.put('k', '5'); KEYPAD_MAP.put('l', '5');
+        KEYPAD_MAP.put('J', '5'); KEYPAD_MAP.put('K', '5'); KEYPAD_MAP.put('L', '5');
+
+        KEYPAD_MAP.put('m', '6'); KEYPAD_MAP.put('n', '6'); KEYPAD_MAP.put('o', '6');
+        KEYPAD_MAP.put('M', '6'); KEYPAD_MAP.put('N', '6'); KEYPAD_MAP.put('O', '6');
+
+        KEYPAD_MAP.put('p', '7'); KEYPAD_MAP.put('q', '7'); KEYPAD_MAP.put('r', '7'); KEYPAD_MAP.put('s', '7');
+        KEYPAD_MAP.put('P', '7'); KEYPAD_MAP.put('Q', '7'); KEYPAD_MAP.put('R', '7'); KEYPAD_MAP.put('S', '7');
+
+        KEYPAD_MAP.put('t', '8'); KEYPAD_MAP.put('u', '8'); KEYPAD_MAP.put('v', '8');
+        KEYPAD_MAP.put('T', '8'); KEYPAD_MAP.put('U', '8'); KEYPAD_MAP.put('V', '8');
+
+        KEYPAD_MAP.put('w', '9'); KEYPAD_MAP.put('x', '9'); KEYPAD_MAP.put('y', '9'); KEYPAD_MAP.put('z', '9');
+        KEYPAD_MAP.put('W', '9'); KEYPAD_MAP.put('X', '9'); KEYPAD_MAP.put('Y', '9'); KEYPAD_MAP.put('Z', '9');
+    }
+
 
 
     public static class BT_Filter {
@@ -359,7 +409,7 @@ public class BluetoothNotificationFilter {
 
             if(this.errorCode.equals(777)) {
                 if(mPrefs.getBoolean("bluetoothAppnameToNumber", false)) {
-                    set_sender =  "+499876" + PhoneNumberUtils.stripSeparators(PhoneNumberUtils.convertKeypadLettersToDigits(set_sender.toString()));
+                    set_sender = PhoneNumberUtils.stripSeparators("+499876" + convertKeypadLettersToDigits(set_sender));
                 }
                 else if (!mPrefs.getBoolean("bluetoothAppnameAsText", false)) {
                     set_content = set_sender + ": " + set_content;
