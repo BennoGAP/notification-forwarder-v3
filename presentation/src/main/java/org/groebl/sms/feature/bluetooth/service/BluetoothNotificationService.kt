87745f4143fb1138193926a2eba4a33f8427910b
Benno <15093499+BennoGAP@users.noreply.github.com>
2023-01-03 19:45:13 +0100

    BluetoothNotificationService in Kotlin

--- presentation/src/main/java/org/groebl/sms/feature/bluetooth/service/BluetoothNotificationService.kt
+++ presentation/src/main/java/org/groebl/sms/feature/bluetooth/service/BluetoothNotificationService.kt
@@ -1,122 +1,146 @@
-package org.groebl.sms.feature.bluetooth.service;
+package org.groebl.sms.feature.bluetooth.service
 
-import android.content.BroadcastReceiver;
-import android.content.ContentValues;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
-import android.os.Handler;
-import android.os.Looper;
-import android.preference.PreferenceManager;
-import android.provider.Telephony;
-import android.service.notification.NotificationListenerService;
-import android.service.notification.StatusBarNotification;
-import android.util.Log;
+import android.content.BroadcastReceiver
+import android.content.ContentValues
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Handler
+import android.os.Looper
+import android.preference.PreferenceManager
+import android.provider.Telephony
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import org.groebl.sms.common.util.BluetoothHelper
+import org.groebl.sms.feature.bluetooth.common.BluetoothNotificationFilter.BT_Filter
+import com.vdurmont.emoji.EmojiParser
+import org.groebl.sms.common.util.AutoBluetoothNotificationManager
+import org.groebl.sms.common.util.extensions.BluetoothMessageHelper
+import timber.log.Timber
+import java.lang.Exception
+import java.util.HashSet
 
-import com.vdurmont.emoji.EmojiParser;
+class BluetoothNotificationService : NotificationListenerService() {
 
-import org.groebl.sms.feature.bluetooth.common.BluetoothDatabase;
-import org.groebl.sms.feature.bluetooth.common.BluetoothHelper;
-import org.groebl.sms.feature.bluetooth.common.BluetoothNotificationFilter;
+    private lateinit var mBroadcastReceiver: BroadcastReceiver
 
-import java.util.HashSet;
-import java.util.Set;
-
-
-public class BluetoothNotificationService extends NotificationListenerService {
-
-    private BroadcastReceiver mBroadcastReceiver;
-
-    public void onCreate() {
-        super.onCreate();
-        startMainService();
+    override fun onCreate() {
+        super.onCreate()
+        startMainService()
     }
 
-    public void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(this.mBroadcastReceiver);
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(mBroadcastReceiver)
     }
 
-    public void startMainService() {
-        this.mBroadcastReceiver = new BluetoothReceiver();
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction("android.bluetooth.device.action.ACL_CONNECTED");
-        intentFilter.addAction("android.bluetooth.device.action.ACL_DISCONNECTED");
-        intentFilter.addAction("android.bluetooth.a2dp.profile.action.CONNECTION_STATE_CHANGED");
-        intentFilter.addAction("android.bluetooth.a2dp.profile.action.PLAYING_STATE_CHANGED");
-        registerReceiver(this.mBroadcastReceiver, intentFilter);
+    private fun startMainService() {
+        mBroadcastReceiver = BluetoothReceiver()
+        val intentFilter = IntentFilter().apply {
+            addAction("android.bluetooth.device.action.ACL_CONNECTED")
+            addAction("android.bluetooth.device.action.ACL_DISCONNECTED")
+            addAction("android.bluetooth.a2dp.profile.action.CONNECTION_STATE_CHANGED")
+            addAction("android.bluetooth.a2dp.profile.action.PLAYING_STATE_CHANGED")
+        }
+        registerReceiver(mBroadcastReceiver, intentFilter)
     }
 
-    @Override
-    public void onNotificationPosted(StatusBarNotification sbn) {
-        super.onNotificationPosted(sbn);
-        Log.d("SMSMsg", "onNotificationPosted: " + sbn.getPackageName());
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        super.onNotificationPosted(sbn)
+        Timber.d("onNotificationPosted: ${sbn.packageName}")
+        val mPrefs = PreferenceManager.getDefaultSharedPreferences(applicationContext)
 
-        SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
         //Check if Notification is -clearable-
-        if (!sbn.isClearable()) { return; }
+        if (!sbn.isClearable) {
+            return
+        }
 
         //Check if Bluetooth-Fordward is enabled
-        if (!mPrefs.getBoolean("bluetoothEnabled", false)) { return; }
+        if (!mPrefs.getBoolean("bluetoothEnabled", false)) {
+            return
+        }
 
         //Check if Connected to bluetooth is enabled
-        if(mPrefs.getBoolean("bluetoothOnlyOnConnect", true) && !mPrefs.getBoolean("bluetoothCurrentStatus", false)) { return; }
+        if (mPrefs.getBoolean("bluetoothOnlyOnConnect", true) && !mPrefs.getBoolean("bluetoothCurrentStatus", false)) {
+            return
+        }
 
         //Check if App is on App-Whitelist
-        String pack = sbn.getPackageName();
-        Set<String> appwhitelist = mPrefs.getStringSet("bluetoothApps", new HashSet<>());
-        if (!appwhitelist.contains(pack)) { return; }
+        val pack = sbn.packageName
+        val appwhitelist = mPrefs.getStringSet("bluetoothApps", HashSet())
+        if (!appwhitelist!!.contains(pack)) {
+            return
+        }
 
         //Everything is fine - here we go..
         try {
-            BluetoothNotificationFilter.BT_Filter BtData = new BluetoothNotificationFilter.BT_Filter();
-            BtData.BluetoothFilter(sbn, getApplicationContext());
+            val BtData = BT_Filter()
+            BtData.BluetoothFilter(sbn, applicationContext)
 
             //Ok, now save the Msg
             if (BtData.allData()) {
 
+                //If text is empty - dont proceed
+                val bluetoothText: String = BluetoothHelper.emojiToNiceEmoji(BtData.content, mPrefs.getBoolean("bluetoothEmoji", true))
+                if (bluetoothText.trim().isEmpty()) { return }
+
                 //Check if this msg already exist
-               if (BluetoothDatabase.searchBluetoothNotificationHash(getApplicationContext(), pack, BluetoothHelper.INSTANCE.notificationHash(BtData.getSender(), BtData.getContent()))) { return; }
+                if (BluetoothHelper.isBluetoothHashCached(pack.toString(), BluetoothHelper.notificationHash(BtData.sender, BtData.content))) { return }
 
                 //Enter the Data in the SMS-DB
-                BluetoothHelper.INSTANCE.addBluetoothMessage(
-                        getApplicationContext(),
-                        EmojiParser.removeAllEmojis(BtData.getSender()),
-                        BluetoothHelper.INSTANCE.emojiToNiceEmoji(BtData.getContent(), mPrefs.getBoolean("bluetoothEmoji", true)),
-                        BtData.getSendTime(),
-                        mPrefs.getBoolean("bluetoothRealmMessage", false),
-                        (mPrefs.getBoolean("bluetoothSaveRead", false) && !mPrefs.getBoolean("bluetoothDelayedRead", false)),
-                        BtData.getErrorCode(),
-                        mPrefs.getBoolean("canUseSubId", true),
-                        1);
+                val btMessageHelper = BluetoothMessageHelper()
+                btMessageHelper.addBluetoothMessage(applicationContext,
+                    EmojiParser.removeAllEmojis(BtData.sender),
+                    bluetoothText,
+                    BtData.sendTime,
+                    mPrefs.getBoolean("bluetoothRealmHideMessage", true),
+                    mPrefs.getBoolean("bluetoothSaveRead", false) && !mPrefs.getBoolean("bluetoothDelayedRead", false),
+                    BtData.errorCode,
+                    mPrefs.getBoolean("canUseSubId", true),
+                    0L
+                )
 
                 //Delayed Mark-as-Read
                 if (mPrefs.getBoolean("bluetoothSaveRead", false) && mPrefs.getBoolean("bluetoothDelayedRead", false)) {
-                    ContentValues cv = new ContentValues();
-                    cv.put("read", true);
-
-                    Handler handler = new Handler(Looper.getMainLooper());
-                    handler.postDelayed(() -> getApplicationContext().getContentResolver().update(Telephony.Sms.Inbox.CONTENT_URI, cv, Telephony.Sms.DATE_SENT + " = ? AND (" + Telephony.Sms.ERROR_CODE + " = ? OR " + Telephony.Sms.ERROR_CODE + " = ?)", new String[]{BtData.getSendTime().toString(), "777", "778"}), 500);
+                    val cv = ContentValues()
+                    cv.put(Telephony.Sms.READ, 1)
+                    val handler = Handler(Looper.getMainLooper())
+                    handler.postDelayed({
+                        applicationContext.contentResolver.update(
+                            Telephony.Sms.Inbox.CONTENT_URI,
+                            cv,
+                            Telephony.Sms.DATE_SENT + " = ? AND (" + Telephony.Sms.ERROR_CODE + " = ? OR " + Telephony.Sms.ERROR_CODE + " = ?)",
+                            arrayOf(BtData.sendTime.toString(), "777", "778")
+                        )
+                    }, 500)
                 }
 
+                //Send Android-Auto Notification
+                if (mPrefs.getBoolean("bluetoothAndroidAuto", false)) {
+                    val mAutoNotificationManager = AutoBluetoothNotificationManager(
+                        applicationContext
+                    )
+                    mAutoNotificationManager.notifyUser(
+                        System.currentTimeMillis().toInt(),
+                        if (BtData.hasPhoneNumber()) BtData.sender else BtData.appName,  //SINNVOLL? TODO
+                        BtData.appNameRaw,
+                        EmojiParser.removeAllEmojis(BtData.sender),
+                        BluetoothHelper.emojiToNiceEmoji(BtData.contentRaw, mPrefs.getBoolean("bluetoothEmoji", true)
+                        )
+                    )
+                }
             }
-
-        }
-        catch(Exception e) {
-            e.printStackTrace();
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
-    @Override
-    public void onNotificationRemoved(StatusBarNotification sbn) {
-        super.onNotificationRemoved(sbn);
-        Log.d("SMSMsg", "onNotificationRemoved: " + sbn.getPackageName());
+    override fun onNotificationRemoved(sbn: StatusBarNotification) {
+        super.onNotificationRemoved(sbn)
+        Timber.d("onNotificationRemoved: ${sbn.packageName}")
     }
 
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return START_STICKY;
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        return START_STICKY
     }
-
-
 }
