559562544f9e16dec65ee7333f573789329887f1
Benno <15093499+BennoGAP@users.noreply.github.com>
2023-01-03 18:42:18 +0100

    Make sure Intent.ACTION_BOOT_COMPLETED is called

--- presentation/src/main/java/org/groebl/sms/feature/bluetooth/service/BluetoothBootReceiver.kt
+++ presentation/src/main/java/org/groebl/sms/feature/bluetooth/service/BluetoothBootReceiver.kt
@@ -4,13 +4,15 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import dagger.android.AndroidInjection
-import org.groebl.sms.feature.bluetooth.common.BluetoothHelper
+import org.groebl.sms.common.util.BluetoothHelper
 
 class BluetoothBootReceiver : BroadcastReceiver() {
 
-    override fun onReceive(context: Context, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent) {
         AndroidInjection.inject(this, context)
 
-        BluetoothHelper.checkAndRestartNotificationListener(context)
+        when (intent.action) {
+            Intent.ACTION_BOOT_COMPLETED -> BluetoothHelper.checkAndRestartNotificationListener(context)
+        }
     }
 }
