da45272aa3df11586394b86e8baaa2a0f5046bd6
Benno <15093499+BennoGAP@users.noreply.github.com>
2023-01-04 09:47:50 +0100

    Move some functions from BluetoothHelper to BluetoothMessageHelper Class

--- presentation/src/main/java/org/groebl/sms/common/util/extensions/BluetoothMessageHelper.kt
+++ presentation/src/main/java/org/groebl/sms/common/util/extensions/BluetoothMessageHelper.kt
@@ -3,13 +3,98 @@ package org.groebl.sms.common.util.extensions
 import android.content.ContentValues
 import android.content.Context
 import android.provider.Telephony
+import com.vdurmont.emoji.EmojiParser
 import io.realm.Realm
 import io.realm.Sort
 import org.groebl.sms.compat.TelephonyCompat
+import org.groebl.sms.model.BluetoothForwardCache
 import org.groebl.sms.model.Conversation
 import org.groebl.sms.model.Message
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class BluetoothMessageHelper {
+
+    fun isBluetoothHashCached(app: String, hash: String): Boolean {
+        val realm = Realm.getDefaultInstance()
+        realm.refresh()
+
+        val count = realm.where(BluetoothForwardCache::class.java)
+            .equalTo("app", app)
+            .equalTo("hash", hash)
+            .count()
+
+        //In Database, return true
+        if (count > 0L) { return true; }
+
+        var maxValue: Long = realm.use { realm ->
+            realm.where(BluetoothForwardCache::class.java).max("id")?.toLong() ?: 0L
+        }
+
+        val bluetoothMessage = BluetoothForwardCache().apply {
+            this.app = app
+            this.hash = hash
+            this.date = System.currentTimeMillis()
+            this.id = ++maxValue
+        }
+
+        realm.executeTransaction { realm.insert(bluetoothMessage) }
+        return false
+    }
+
+    fun notificationHash(sender: String, content: String): String {
+        val code = "$content | $sender"
+
+        try {
+            val m = MessageDigest.getInstance("MD5")
+            m.update(code.toByteArray(), 0, code.length)
+            val big = BigInteger(1, m.digest())
+            return String.format("%1$032x", big)
+        } catch (e: NoSuchAlgorithmException) {
+            return code.substring(0, 31)
+        }
+    }
+
+    fun emojiToNiceEmoji(text: String, active: Boolean): String {
+        var text = text
+        /*
+        text = text.replace("[\ud83d\ude42]".toRegex(), ":)")
+        text = text.replace("[\ud83d\ude0a]".toRegex(), ":)")
+        text = text.replace("[\ud83d\ude09]".toRegex(), ";)")
+        text = text.replace("[\ud83d\ude00]".toRegex(), ":D")
+        text = text.replace("[\ud83d\ude03]".toRegex(), ":D")
+        text = text.replace("[\ud83d\ude04]".toRegex(), ":D")
+        text = text.replace("[\ud83d\ude2c]".toRegex(), "=D")
+        text = text.replace("[\ud83d\ude01]".toRegex(), "=D")
+        text = text.replace("[\ud83d\ude0b]".toRegex(), ":P")
+        text = text.replace("[\ud83d\ude1b]".toRegex(), ":P")
+        text = text.replace("[\ud83d\ude1c]".toRegex(), ";P")
+        text = text.replace("[\ud83d\ude1d]".toRegex(), ";P")
+        text = text.replace("[\ud83d\ude41]".toRegex(), ":(")
+        text = text.replace("[\u2639]".toRegex(), ":(")
+        text = text.replace("[\ud83d\ude10]".toRegex(), ":|")
+        text = text.replace("[\ud83d\ude11]".toRegex(), ":|")
+        */
+        text = text.replace("[\ud83d\udc9a]".toRegex(), "<3")
+        text = text.replace("[\ud83d\udc9b]".toRegex(), "<3")
+        text = text.replace("[\ud83d\udc9c]".toRegex(), "<3")
+        text = text.replace("[\ud83d\udc99]".toRegex(), "<3")
+        text = text.replace("[\u2764]".toRegex(), "<3")
+        text = text.replace("[\ud83d\udc94]".toRegex(), "</3")
+        //TODO: find more
+
+        //text = text.replaceAll("[\u25a1]", ""); // [] =>
+        /*
+            String output = "";
+            for (int i = 0; i < text.length(); i++) {
+                output = output + ":" + Integer.toHexString(text.charAt(i));
+            }
+            Log.d("output", output);
+        */
+
+        return if (active) EmojiParser.parseToAliases(text, EmojiParser.FitzpatrickAction.REMOVE) else text
+    }
 
     fun addBluetoothMessage(context: Context, address: String, body: String, sentTime: Long, hideInRealm: Boolean = true, asRead: Boolean = false, errorCode: Int, canUseSubId: Boolean = true, subId: Long = 0L) {
         if(!hideInRealm) {
