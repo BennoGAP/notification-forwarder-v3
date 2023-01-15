b077db42fd1588f6d7738632cd8129a9a8442ccd
Benno <15093499+BennoGAP@users.noreply.github.com>
2023-01-15 18:19:29 +0100

    Fixed BluetoothSettingsState, otherwise Permission is checked wrong

--- presentation/src/main/java/org/groebl/sms/feature/bluetooth/BluetoothSettingsState.kt
+++ presentation/src/main/java/org/groebl/sms/feature/bluetooth/BluetoothSettingsState.kt
@@ -2,8 +2,8 @@ package org.groebl.sms.feature.bluetooth
 
 data class BluetoothSettingsState(
         val bluetooth_enabled: Boolean = false,
-        val bluetooth_only_on_connect: Boolean = true,
-        val bluetooth_autodelete: Boolean = true,
+        val bluetooth_only_on_connect: Boolean = false,
+        val bluetooth_autodelete: Boolean = false,
 
         val bluetooth_save_read: Boolean = false,
         val bluetooth_delayed_read: Boolean = false,
@@ -11,13 +11,13 @@ data class BluetoothSettingsState(
         val bluetooth_appname_as_sender_text: Boolean = false,
         val bluetooth_appname_as_sender_number: Boolean = false,
 
-        val bluetooth_whatsapp_to_contact: Boolean = true,
+        val bluetooth_whatsapp_to_contact: Boolean = false,
         val bluetooth_whatsapp_hide_prefix: Boolean = true,
 
-        val bluetooth_telegram_to_contact: Boolean = true,
+        val bluetooth_telegram_to_contact: Boolean = false,
         val bluetooth_telegram_hide_prefix: Boolean = true,
 
-        val bluetooth_signal_to_contact: Boolean = true,
+        val bluetooth_signal_to_contact: Boolean = false,
         val bluetooth_signal_hide_prefix: Boolean = true,
 
         val bluetooth_max_vol: Boolean = false,
