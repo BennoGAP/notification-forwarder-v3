deea96b04e9e07dc0e42515afa2556be656d5ea9
Benno <15093499+BennoGAP@users.noreply.github.com>
2026-01-11 12:09:59 +0100

    Fix for NullPointerException - subscriptionInfo.displayName must not be null

--- data/src/main/java/org/groebl/sms/compat/SubscriptionInfoCompat.kt
+++ data/src/main/java/org/groebl/sms/compat/SubscriptionInfoCompat.kt
@@ -26,6 +26,6 @@ data class SubscriptionInfoCompat(private val subscriptionInfo: SubscriptionInfo
 
     val simSlotIndex get() = subscriptionInfo.simSlotIndex
 
-    val displayName: CharSequence get() = subscriptionInfo.displayName
+    val displayName: CharSequence get() = subscriptionInfo.displayName ?: ""
 
 }
